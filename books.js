bec137ae2fee6050044061e9deeff5cb11e2fadd
osamnvdtv-lab <osamnvdtv@gmail.com>
2026-04-26 20:00:49 +0300

    Add files via upload

---
+++ books.js
@@ -0,0 +1,44 @@
+const booksData = {
+  "prep_3": [
+    {
+      name: "علوم – ثالثة إعدادي",
+      img: "Al-Dahih1.jpg",
+      link: "Al-Dahih1.pdf"
+    },
+    {
+      name: "الرياضيات – ثالثة إعدادي",
+      img: "Al-Dahih3.jpg",
+      link: "Al-Dahih3.pdf"
+    },
+    {
+      name: "دراسات – ثالثة إعدادي",
+      img: "Al-Dahih2.jpg",
+      link: "Al-Dahih2.pdf"
+    },
+    {
+      name: "تكنولوجيا – ثالثة إعدادي",
+      img: "Al-Dahih4.jpg",
+      link: "Al-Dahih4.pdf"
+    },
+    {
+      name: "اللغة العربية الأزهرية – ثالثة إعدادي",
+      img: "Al-Dahih5.jpg",
+      link: "Al-Dahih5.pdf"
+    },
+    {
+      name: "الفقه الحنفي – ثالثة إعدادي",
+      img: "Al-Dahih6.jpg",
+      link: "Al-Dahih6.pdf"
+    },
+    {
+      name: "أصول الدين – ثالثة إعدادي",
+      img: "Al-Dahih7.jpg",
+      link: "Al-Dahih7.pdf"
+    },
+    {
+      name: "اللغة العربية – ثالثة إعدادي",
+      img: "Al-Dahih8.jpg",
+      link: "Al-Dahih8.pdf"
+    },
+  ]
+};
---
+++ reviews.js
@@ -0,0 +1,24 @@
+const reviewsData = {
+  "prep_3": [
+    {
+      subject: "رياضيات",
+      name: "نماذج الرياضيات",
+      img: "img/Al-Dahih11.jpg",
+      link: "book/Al-Dahih11.pdf"
+      
+    },
+    {
+      subject: "رياضيات",
+      name: "مراجعة الرياضيات",
+      img: "Al-Dahih12.jpg",
+      link: "Al-Dahih12.pdf"
+      
+    },
+    {
+      subject: "علوم",
+      name: "مراجعة العلوم",
+      img: "images/science_review.jpg",
+      link: "reviews/prep_3/science.pdf"
+    }
+  ]
+};
